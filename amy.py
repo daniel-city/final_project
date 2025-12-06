84b7d7aa57ba38cb4f06f20182745ef013e22acd
Amy <amyboev@umich.edu>
2025-12-06 10:59:30 -0500

    Update comments in amy.py for clarity on branch pushing

--- amy.py
+++ amy.py
@@ -1,1 +1,3 @@
 #putting in a comment so i can commit this file and see if it goes into our group repo
+#ok it pushed to the "master" branch, but I want to push to the "main" branch
+#this is a test to see if I can push to the main branch
