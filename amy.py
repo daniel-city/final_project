949bc717eac3bbad616de6084461e8cfab5fd3d5
Amy <amyboev@umich.edu>
2025-12-07 11:58:33 -0500

    initialize amy.py with necessary imports and configuration variables

--- amy.py
+++ amy.py
@@ -1,1 +1,12 @@
-#testing to see if this commits to the main branch finally :,(
+import json
+import sqlite3
+import os
+import requests
+
+API_KEY = "bd1842990d39e66f7830f18d756cb443636008b7"
+
+top_cities_file = "top100_cities.json"
+raw_json_file = "aqi_data.json"
+db_file = "air_quality.db"
+
+batch_size = 25
