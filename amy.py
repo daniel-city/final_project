d9c25832144637668b27cef847d08f4eabdae2ee
Amy <amyboev@umich.edu>
2025-12-08 11:42:53 -0500

    changed coordinates to not duplicate + debugging aqi functions

--- amy.py
+++ amy.py
@@ -11,97 +11,97 @@ db_file = "test.db"
 
 batch_size = 25
 
-coordinates = [
-    '40.7128,-74.0060',    # New York, NY
-    '34.0522,-118.2437',   # Los Angeles, CA
-    '41.8781,-87.6298',    # Chicago, IL
-    '29.7604,-95.3698',    # Houston, TX
-    '33.4484,-112.0740',   # Phoenix, AZ
-    '39.9526,-75.1652',    # Philadelphia, PA
-    '29.4241,-98.4936',    # San Antonio, TX
-    '32.7157,-117.1611',   # San Diego, CA
-    '32.7767,-96.7970',    # Dallas, TX
-    '37.3382,-121.8863',   # San Jose, CA
-    '30.2672,-97.7431',    # Austin, TX
-    '30.3322,-81.6557',    # Jacksonville, FL
-    '32.7555,-97.3308',    # Fort Worth, TX
-    '39.9612,-82.9988',    # Columbus, OH
-    '35.2271,-80.8431',    # Charlotte, NC
-    '37.7749,-122.4194',   # San Francisco, CA
-    '39.7684,-86.1581',    # Indianapolis, IN
-    '47.6062,-122.3321',   # Seattle, WA
-    '39.7392,-104.9903',   # Denver, CO
-    '38.9072,-77.0369',    # Washington, DC
-    '42.3601,-71.0589',    # Boston, MA
-    '31.7619,-106.4850',   # El Paso, TX
-    '36.1627,-86.7816',    # Nashville, TN
-    '42.3314,-83.0458',    # Detroit, MI
-    '35.4676,-97.5164',    # Oklahoma City, OK
-    '45.5051,-122.6750',   # Portland, OR
-    '36.1699,-115.1398',   # Las Vegas, NV
-    '35.1495,-90.0490',    # Memphis, TN
-    '38.2527,-85.7585',    # Louisville, KY
-    '39.2904,-76.6122',    # Baltimore, MD
-    '43.0389,-87.9065',    # Milwaukee, WI
-    '35.0844,-106.6504',   # Albuquerque, NM
-    '32.2226,-110.9747',   # Tucson, AZ
-    '36.7378,-119.7871',   # Fresno, CA
-    '38.5816,-121.4944',   # Sacramento, CA
-    '33.4152,-111.8315',   # Mesa, AZ
-    '33.7490,-84.3880',    # Atlanta, GA
-    '41.2565,-95.9345',    # Omaha, NE
-    '38.8339,-104.8214',   # Colorado Springs, CO
-    '35.7796,-78.6382',    # Raleigh, NC
-    '36.8529,-75.9780',    # Virginia Beach, VA
-    '25.7617,-80.1918',    # Miami, FL
-    '33.7701,-118.1937',   # Long Beach, CA
-    '37.8044,-122.2711',   # Oakland, CA
-    '44.9778,-93.2650',    # Minneapolis, MN
-    '39.1031,-84.5120',    # Cincinnati, OH
-    '43.0731,-89.4012',    # Madison, WI
-    '35.0456,-85.3097',    # Chattanooga, TN
-    '39.7397,-75.5390',    # Camden, NJ
-    '32.6781,-83.2220',    # Augusta, GA
-    '29.9511,-90.0715',    # New Orleans, LA
-    '36.1745,-86.7670',    # Nashville, TN (duplicate removed later)
-    '30.6954,-88.0399',    # Mobile, AL
-    '39.9610,-82.9988',    # Columbus, OH (duplicate removed later)
-    '40.4406,-79.9959',    # Pittsburgh, PA
-    '43.6532,-79.3832',    # Toronto, ON (optional non-US)
-    '40.7357,-74.1724',    # Newark, NJ
-    '39.9612,-82.9988',    # Columbus, OH (duplicate)
-    '42.1015,-72.5898',    # Springfield, MA
-    '36.1627,-86.7816',    # Nashville, TN (duplicate)
-    '33.5207,-86.8025',    # Birmingham, AL
-    '29.4241,-98.4936',    # San Antonio, TX (duplicate)
-    '32.7765,-79.9311',    # Charleston, SC
-    '38.6270,-90.1994',    # St. Louis, MO
-    '35.2271,-80.8431',    # Charlotte, NC (duplicate)
-    '35.1495,-90.0490',    # Memphis, TN (duplicate)
-    '36.0972,-79.7745',    # Greensboro, NC
-    '40.7306,-73.9352',    # Queens, NY
-    '39.7684,-86.1581',    # Indianapolis, IN (duplicate)
-    '41.6005,-93.6091',    # Des Moines, IA
-    '36.7783,-119.4179',   # California (approximate center)
-    '40.6501,-73.9496',    # Brooklyn, NY
-    '33.4484,-112.0740',   # Phoenix, AZ (duplicate)
-    '34.0007,-81.0348',    # Columbia, SC
-    '42.3314,-83.0458',    # Detroit, MI (duplicate)
-    '30.3322,-81.6557',    # Jacksonville, FL (duplicate)
-    '39.9612,-82.9988',    # Columbus, OH (duplicate)
-    '38.5816,-121.4944',   # Sacramento, CA (duplicate)
-    '36.7468,-119.7726',   # Fresno, CA (duplicate)
-    '40.4406,-79.9959',    # Pittsburgh, PA (duplicate)
-    '42.3601,-71.0589',    # Boston, MA (duplicate)
-    '39.7684,-86.1581',    # Indianapolis, IN (duplicate)
-    '32.7555,-97.3308',    # Fort Worth, TX (duplicate)
-    '30.2672,-97.7431',    # Austin, TX (duplicate)
-    '32.7157,-117.1611',   # San Diego, CA (duplicate)
-    '47.6062,-122.3321',   # Seattle, WA (duplicate)
-    '39.7392,-104.9903',   # Denver, CO (duplicate)
-    '38.9072,-77.0369',    # Washington, DC (duplicate)
-    '34.0522,-118.2437',   # Los Angeles, CA (duplicate)
-    '40.7128,-74.0060'     # New York, NY (duplicate)
+coordinate_points = [
+    "40.7128,-74.0060",  # New York, NY
+    "34.0522,-118.2437",  # Los Angeles, CA
+    "41.8781,-87.6298",  # Chicago, IL
+    "29.7604,-95.3698",  # Houston, TX
+    "33.4484,-112.0740",  # Phoenix, AZ
+    "39.9526,-75.1652",  # Philadelphia, PA
+    "29.4241,-98.4936",  # San Antonio, TX
+    "32.7157,-117.1611",  # San Diego, CA
+    "32.7767,-96.7970",  # Dallas, TX
+    "37.3382,-121.8863",  # San Jose, CA
+    "30.2672,-97.7431",  # Austin, TX
+    "30.3322,-81.6557",  # Jacksonville, FL
+    "32.7555,-97.3308",  # Fort Worth, TX
+    "39.9612,-82.9988",  # Columbus, OH
+    "35.2271,-80.8431",  # Charlotte, NC
+    "37.7749,-122.4194",  # San Francisco, CA
+    "39.7684,-86.1581",  # Indianapolis, IN
+    "47.6062,-122.3321",  # Seattle, WA
+    "39.7392,-104.9903",  # Denver, CO
+    "38.9072,-77.0369",  # Washington, DC
+    "42.3601,-71.0589",  # Boston, MA
+    "31.7619,-106.4850",  # El Paso, TX
+    "36.1627,-86.7816",  # Nashville, TN
+    "42.3314,-83.0458",  # Detroit, MI
+    "35.4676,-97.5164",  # Oklahoma City, OK
+    "45.5051,-122.6750",  # Portland, OR
+    "36.1699,-115.1398",  # Las Vegas, NV
+    "35.1495,-90.0490",  # Memphis, TN
+    "38.5816,-121.4944",  # Sacramento, CA
+    "38.2527,-85.7585",  # Louisville, KY
+    "39.2904,-76.6122",  # Baltimore, MD
+    "43.0389,-87.9065",  # Milwaukee, WI
+    "35.0844,-106.6504",  # Albuquerque, NM
+    "36.7378,-119.7871",  # Fresno, CA
+    "32.2226,-110.9747",  # Tucson, AZ
+    "41.2565,-95.9345",  # Omaha, NE
+    "38.8339,-104.8214",  # Colorado Springs, CO
+    "36.8529,-75.9780",  # Virginia Beach, VA
+    "35.7796,-78.6382",  # Raleigh, NC
+    "25.7617,-80.1918",  # Miami, FL
+    "43.6532,-79.3832",  # Toronto, ON
+    "45.5017,-73.5673",  # Montreal, QC
+    "49.2827,-123.1207",  # Vancouver, BC
+    "51.0447,-114.0719",  # Calgary, AB
+    "53.5461,-113.4938",  # Edmonton, AB
+    "45.4215,-75.6972",  # Ottawa, ON
+    "43.5890,-79.6441",  # Mississauga, ON
+    "49.8951,-97.1384",  # Winnipeg, MB
+    "44.6488,-63.5752",  # Halifax, NS
+    "43.4501,-80.4832",  # Kitchener, ON
+    "45.4112,-75.6981",  # Gatineau, QC
+    "52.2681,-113.8112",  # Red Deer, AB
+    "50.4452,-104.6189",  # Regina, SK
+    "53.9140,-122.7497",  # Prince George, BC
+    "43.2557,-79.8711",  # Hamilton, ON
+    "43.1610,-79.2440",  # St. Catharines, ON
+    "42.8864,-78.8784",  # Buffalo, NY
+    "27.9506,-82.4572",  # Tampa, FL
+    "40.4406,-79.9959",  # Pittsburgh, PA
+    "39.1031,-84.5120",  # Cincinnati, OH
+    "41.4993,-81.6944",  # Cleveland, OH
+    "44.9778,-93.2650",  # Minneapolis, MN
+    "29.9511,-90.0715",  # New Orleans, LA
+    "33.5207,-86.8025",  # Birmingham, AL
+    "32.3513,-95.3011",  # Tyler, TX
+    "45.0275,-93.3655",  # Brooklyn Park, MN
+    "30.4383,-84.2807",  # Tallahassee, FL
+    "33.7488,-84.3880",  # Atlanta, GA
+    "34.0007,-81.0348",  # Columbia, SC
+    "21.3069,-157.8583",  # Honolulu, HI
+    "47.2529,-122.4443",  # Tacoma, WA
+    "40.7608,-111.8910",  # Salt Lake City, UT
+    "39.0997,-94.5786",  # Kansas City, MO
+    "33.6846,-117.8265",  # Irvine, CA
+    "35.3733,-119.0187",  # Bakersfield, CA
+    "30.6954,-88.0399",  # Mobile, AL
+    "40.0140,-105.2705",  # Boulder, CO
+    "43.0481,-76.1474",  # Syracuse, NY
+    "39.5296,-119.8138",  # Reno, NV
+    "32.7765,-79.9311",  # Charleston, SC
+    "40.8258,-74.2090",  # Newark, NJ
+    "33.3062,-111.8413",  # Chandler, AZ 
+    "41.6639,-83.5552",  # Toledo, OH 
+    "44.9537,-93.0900",  # St. Paul, MN 
+    "26.0078,-80.2963",  # Pembroke Pines, FL 
+    "42.6526,-73.7562",  # Albany, NY
+    "45.2733,-66.0633",  # Saint John, NB
+    "52.8390,-104.6060",  # Melfort, SK
+    "35.9940,-78.8986",  # Durham, NC
+    "37.5407,-77.4360",  # Richmond, VA
 ]
 
 def init_db():
@@ -169,7 +169,7 @@ def get_top_cities_aqi():
     cur.execute("SELECT COUNT(*) FROM aqi_results")
     completed = cur.fetchone()[0]
 
-    next_coords = coordinates[completed: completed + batch_size]
+    next_coords = coordinate_points[completed: completed + batch_size]
 
     added = 0
 
